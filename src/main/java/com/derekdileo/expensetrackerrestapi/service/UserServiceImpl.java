2d1ab773f94a3b555580537d4a110138e3cb08be
Derek DiLeo <derek@Dereks-Mac-mini.local>
2022-05-28 14:27:15 -0400

    Ceate method to get logged in User using SecurityContextHolder.

--- src/main/java/com/derekdileo/expensetrackerrestapi/service/UserServiceImpl.java
+++ src/main/java/com/derekdileo/expensetrackerrestapi/service/UserServiceImpl.java
@@ -7,6 +7,9 @@ import com.derekdileo.expensetrackerrestapi.exceptions.ResourceNotFoundException
 import com.derekdileo.expensetrackerrestapi.repository.UserRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -74,6 +77,16 @@ public class UserServiceImpl implements UserService{
     public void deleteUser(Long id) {
         User existingUser = readUserById(id);
         userRepo.delete(existingUser);
+    }
+
+    @Override
+    public User getLoggedInUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        String email = authentication.getName();
+
+        return userRepo.findByEmail(email).orElseThrow(() ->
+                new UsernameNotFoundException("User not found for email: " + email));
     }
 
 }
